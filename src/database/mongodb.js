31fad5bc2dbcb8c768a25020291bbefcdeff19c6
Renan <renanandreolla@outlook.com>
2025-04-27 23:04:23 -0300

    up

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,28 +1,17 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const uri = process.env.DBURI;
-
-// mongodb+srv://renan:<db_password>@atcluster.r3nia1r.mongodb.net/
-
-const client = new MongoClient(uri, {
+const client = new MongoClient(process.env.DBURI, {
   serverApi: ServerApiVersion.v1,
+  tls: true,
+  tlsAllowInvalidCertificates: false,
 });
 
 const databaseConn = async () => {
   try {
-    await client
-      .connect()
-      .then(() => {
-        console.log("Connected successfully to MongoDB");
-      })
-      .catch((err) => {
-        console.log("URI: ", uri);
-
-        console.log("Error on connect to MongoDB: ", err);
-      });
+    await client.connect();
+    console.log("Connected successfully to MongoDB");
   } catch (err) {
-    client.close();
-    console.log("Error on connect database: ", err);
+    console.error("Error on connect to MongoDB:", err);
   }
 };
 
